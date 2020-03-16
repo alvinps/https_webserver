bc2d22af86a59c1de987fde310db31b856829249
Alvin Poudel Sharma <alvin.poudelsharma@mavs.uta.edu>
2020-03-16 15:09:15 -0500

    web_server created

--- web_server.py
+++ web_server.py
@@ -1,35 +1,44 @@
-import socket module
+
 from socket import *
+import sys
+Max_Connection_Allowed = 10
+
 serverSocket = socket(AF_INET, SOCK_STREAM)
-#Prepare a sever socket
-#Fill in start
-#Fill in end
+Port_num = 8080
+if (len(sys.argv) >= 2):
+    Port_num = int(sys.argv[1])
+print(Port_num)
+print("TEst")
+
+serverSocket.bind(('',Port_num))
+serverSocket.listen(Max_Connection_Allowed)
+
 while True:
-#Establish the connection
-print 'Ready to serve...'
-connectionSocket, addr =
-#Fill in start
-#Fill in end
-try:
-message =
-#Fill in start
-#Fill in end
-filename = message.split()[1]
-f = open(filename[1:])
-outputdata = #Fill in start
-#Fill in end
-#Send one HTTP header line into socket
-#Fill in start
-#Fill in end
-#Send the content of the requested file to the client
-for i in range(0, len(outputdata)):
-    connectionSocket.send(outputdata[i])
-connectionSocket.close()
-except IOError:
-#Send response message for file not found
-#Fill in start
-#Fill in end
-#Close client socket
-#Fill in start
-#Fill in end
+    #Establish the connection
+    print("Ready to serve...")
+    connectionSocket, addr = serverSocket.accept()
+    # print out details about the connection here.
+    #Fill in start
+    #Fill in end
+    filename = ""
+    try:
+        message = connectionSocket.recv(1024)
+        if message != '':
+            filename = message.split()[1]
+
+        filename = message.split()[1]
+        try:
+            file = open(filename[1:])
+            outputdata = file.read()
+            connectionSocket.send(bytes("HTTP/1.1 200 OK\n","UTF-8"))
+            for i in range(0, len(outputdata)):
+                connectionSocket.send(bytes(outputdata[i],"UTF-8"))
+            connectionSocket.close()
+        except IOError:
+            connectionSocket.send(bytes("HTTP/1.1 404 Not Found\n","UTF-8"))
+            connectionSocket.close()
+    except RuntimeError:
+        connectionSocket.send(bytes("HTTP/1.1 400 Bad Request\n","UTF-8"))
+    connectionSocket.close()
+
 serverSocket.close()
