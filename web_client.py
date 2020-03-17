e2f620544e3528c2cc5783c8fee7d5d22b2baaf9
Alvin Poudel Sharma <alvin.poudelsharma@mavs.uta.edu>
2020-03-17 16:30:45 -0500

    Comments added to the files for readability

--- web_client.py
+++ web_client.py
@@ -1,13 +1,20 @@
+# WEB SERVER
+# Alvin Poudel Sharma
+# UTA ID: 1001555230
 
 from socket import *
 import sys
-
+# socket and sys in modules imported
 
 clientSocket = socket(AF_INET, SOCK_STREAM)
+# default port number for connection if not provided.
 Port_num = 8080
 filename = "index.htm"
+# default file name if no provided
 ip_address = str(sys.argv[1])
 param_count  = len(sys.argv)
+
+# parameters received form the command line for the operation of the connection.
 if param_count == 4:
     filename = str(sys.argv[3])
     Port_num = int(sys.argv[2])
@@ -17,13 +24,16 @@ elif param_count ==3:
     except ValueError:
         filename = str(sys.argv[2])
 
-
+# client connected to the server.
 clientSocket.connect((ip_address, Port_num))
+# client connection initiated to the server.
 message ="GET /"+filename+" HTTP/1.1\n This came from the python client\n"
+# message to be sent to the server.
 clientSocket.send(bytes(message,"UTF-8"))
 print("The following request was sent to the server: %s" % message)
 print("Message Received: \n\n ")
 while True:
+    # response received from the server
     Response_received = clientSocket.recv(2048)
     if Response_received.decode("UTF-8") == "":
         break
@@ -31,4 +41,5 @@ while True:
         print("%s" % Response_received.decode("UTF-8"))
 
 print("\nClosing connection...")
+# connection closed.
 clientSocket.close()
--- web_server.py
+++ web_server.py
@@ -1,14 +1,21 @@
+# WEB SERVER
+# Alvin Poudel Sharma
+# UTA ID: 1001555230
 
 from socket import *
 import sys
+# socket and sys in modules imported
 Max_Connection_Allowed = 10
 
+# server socket initiated
 serverSocket = socket(AF_INET, SOCK_STREAM)
-Port_num = 8080
+Port_num = 8080 # deafult port  number to be used
+# if provided in the commandline that specific port number is used
 if (len(sys.argv) >= 2):
-    Port_num = int(sys.argv[1])
+    Port_num = int(sys.argv[1]) #
 
 serverSocket.bind(('',Port_num))
+# listening for a connection from a client
 serverSocket.listen(Max_Connection_Allowed)
 print("Connection ready on port %d...\n" % Port_num)
 print("*** A Simple Web Server ***")
@@ -17,7 +24,8 @@ while True:
     #Establish the connection
     print("\n\n***************************************************************")
     print("Ready to serve...\n")
-    connectionSocket, addr = serverSocket.accept()
+    connectionSocket, addr = serverSocket.accept() # new connection accepted
+
     # print out details about the connection here.
     print("Connection Accepted by:\n")
     print("Client Host: %s" % gethostbyaddr(connectionSocket.getpeername()[0])[0])
@@ -25,12 +33,14 @@ while True:
 
     try:
         message = connectionSocket.recv(1024)
+        # new message request received from client
         print("\n\nMessage received: \n%s\n" % (message.decode("utf-8")))
         if message == '':
+            # if the message request is empty
             print("Response sent: Bad Request 400")
             connectionSocket.send(bytes("HTTP/1.1 400 Bad Request\n","UTF-8"))
-            connectionSocket.close()
         else:
+            # if a file is requested by the client
             filename = message.split()[1]
             if filename == bytes("/","UTF-8"):
                 filename = bytes("/index.htm","UTF-8")
@@ -41,12 +51,16 @@ while True:
             connectionSocket.send(bytes("HTTP/1.1 200 OK","UTF-8"))
             print("File ( %s ) is being sent..." % (filename.decode("UTF-8")))
             for i in range(0, len(outputdata)):
+                # sending all the data from the file to the client.
                 connectionSocket.send(bytes(outputdata[i],"UTF-8"))
     except IOError:
+        # if the file is not found then NOT FOUND is sent to the client
         connectionSocket.send(bytes("HTTP/1.1 404 Not Found\n","UTF-8"))
         print("Response sent: File Not Found 404")
 
     connectionSocket.close()
+    # closing the connection
     print("Closing connection... ")
 
+# closing the server
 serverSocket.close()
